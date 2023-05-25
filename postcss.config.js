c6fd0db6cfc5c6560f54d00ab18f092b81e842aa
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-05-25 14:14:09 -0300

    fix: fixed post css configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  tailwindConfig: "./tailwind.config.js",
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
