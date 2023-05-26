e34e495bca07f3f9efddd865077ed5ed8cd8b9b6
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-05-26 10:28:24 -0300

    feat: loading dynamic component and mapped chess pieces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import BoxWrapper from "@/components/common/boxWrapper";
+import MappedPieces from "@/components/selectChessPieces/mappedPieces";
+import { piecesDataType } from "@/types/piecesTypes";
+import dynamic from "next/dynamic";
+
+const getDados = async (): Promise<piecesDataType[]> => {
+  const res = await fetch("http://localhost:5000/api/pieces");
+  return res.json();
+};
+
+const DynamicContainer = dynamic(
+  () => import("../components/common/container"),
+  {
+    ssr: false,
+    loading: () => <p>Loading page soon...</p>,
+  }
+);
+
+export default async function Home() {
+  const data = await getDados();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1></h1>
-    </main>
+    <>
+      <main className="mx-auto flex w-90% justify-center">
+        <DynamicContainer>
+          <BoxWrapper
+            title="Passo 01 - Selecione Peças"
+            element={<MappedPieces chessPieces={data} />}
+          />
+        </DynamicContainer>
+      </main>
+    </>
   );
 }
